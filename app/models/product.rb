bf2d8907bfb222284533d86fc5b20345d9443db3
Michael Dooney <mail@michaeldooney.net>
2019-01-05 20:36:05 +0100

    5.2.1 update to search bar for ignoring character cases

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ApplicationRecord
-  def self.search(search_term)
-    Product.where("name LIKE ?", "%#{search_term}%")
-  end
+	def self.search(search_term)
+		if Rails.env.production? # use "ilike" matching operator for postgres to ignore upper- and lower-case
+			Product.where("name ilike ?", "%#{search_term}%")
+		else
+			Product.where("name LIKE ?", "%#{search_term}%")
+		end
+	end
 end
