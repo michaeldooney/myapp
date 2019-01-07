9e7cd912d88d5968bca93257178643d17cc65449
Michael Dooney <mail@michaeldooney.net>
2019-01-07 21:19:40 +0100

    5.7 The Comment Model, FontAwesome Gem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,17 @@
 class Product < ApplicationRecord
+	has_many :comments
+	has_many :orders
 	def self.search(search_term)
 		if Rails.env.production? # use "ilike" matching operator for postgres to ignore upper- and lower-case
 			Product.where("name ilike ?", "%#{search_term}%")
 		else
 			Product.where("name LIKE ?", "%#{search_term}%")
 		end
+	end
+	def highest_rating_comment
+  	comments.rating_desc.first
+	end
+	def lowest_rating_comment
+		comments.rating_asc.first
 	end
 end
