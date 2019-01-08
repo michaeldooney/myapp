a6ac56c0dc2965a2340618942757bc20b61e08c0
Michael Dooney <mail@michaeldooney.net>
2019-01-08 14:36:26 +0100

    5.8 Comments Controller & Views, RatyRate stars

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
   resources :users
-  resources :products
+  resources :products do
+    resources :comments
+  end
+  resources :orders, only: [:index, :show, :create, :destroy]
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  root 'simple_pages#landing_page'
-  resources :orders, only: [:index, :show, :create, :destroy]
   post 'simple_pages/thank_you'
+  root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
