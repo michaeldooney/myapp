7ec32ac0723c31bad9705a9c97ab0aaec9b51b95
Michael Dooney <mail@michaeldooney.net>
2019-01-23 14:54:56 +0100

    6.5.1 changed price from decimal to integer

---
+++ db/migrate/20190123130541_change_price_from_decimal_to_integer.rb
@@ -0,0 +1,9 @@
+class ChangePriceFromDecimalToInteger < ActiveRecord::Migration[5.2]
+  def up
+    change_column :products, :price, :integer
+  end
+
+  def down
+    change_column :products, :price, :decimal
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_10_175729) do
+ActiveRecord::Schema.define(version: 2019_01_23_130541) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2019_01_10_175729) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "colour"
-    t.decimal "price"
+    t.integer "price"
   end
 
   create_table "users", force: :cascade do |t|
